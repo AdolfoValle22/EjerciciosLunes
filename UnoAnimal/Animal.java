a9756774f273ffefb33e447c66f3c0d241919b5b
Josue <josueadolfov4lle@gmail.com>
2025-05-25 17:22:47 -0600

    Se creo el ejercicio "Animal"

---
+++ UnoAnimal/Animal.java
@@ -0,0 +1,57 @@
+package UnoAnimal;
+
+public class Animal {
+private String nombre;
+private String orden;
+private int extremidades;
+
+
+public Animal() {
+}
+
+
+public Animal(String nombre, String orden, int extremidades) {
+    this.nombre = nombre;
+    this.orden = orden;
+    this.extremidades = extremidades;
+}
+
+
+public String getNombre() {
+    return nombre;
+}
+
+
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+
+
+public String getOrden() {
+    return orden;
+}
+
+
+public void setOrden(String orden) {
+    this.orden = orden;
+}
+
+
+public int getExtremidades() {
+    return extremidades;
+}
+
+
+public void setExtremidades(int extremidades) {
+    this.extremidades = extremidades;
+}
+
+public void mostrarDatos() {
+    System.out.println("Nombre del animal: " + nombre);
+    System.out.println("Orden: " + orden);
+    System.out.println("Número de extremidades: " + extremidades);
+    System.out.println("---------------------------");
+
+}
+
+}
---
+++ UnoAnimal/Main.java
@@ -0,0 +1,15 @@
+package UnoAnimal;
+
+public class Main {
+public static void main(String[] args) {
+
+Animal animal1 = new Animal("Tigre", "Carnívoro", 4);
+Animal animal2 = new Animal("Águila", "Ave rapaz", 2);
+Animal animal3 = new Animal("Araña", "Arácnido", 8);
+
+    animal1.mostrarDatos();
+    animal2.mostrarDatos();
+    animal3.mostrarDatos();
+}
+
+}
